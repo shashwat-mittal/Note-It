d9770882220c1f3f1933bffaace860092229f03d
Shashwat Mittal <shashwatmittal.india@gmail.com>
2021-10-26 23:58:04 +0530

    updated

--- app/src/main/java/com/example/noteit/addnote.java
+++ app/src/main/java/com/example/noteit/addnote.java
@@ -3,10 +3,12 @@ package com.example.noteit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.Toolbar;
 
 public class addnote extends AppCompatActivity {
     Toolbar toolbar;
+    EditText notetitle, notedetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +16,9 @@ public class addnote extends AppCompatActivity {
         setContentView(R.layout.activity_addnote);
         toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        notedetails=findViewById(R.id.notedetails);
+        notetitle=findViewById(R.id.notetitle);
     }
 
 }
