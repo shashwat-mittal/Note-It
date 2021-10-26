7bd286e92fdfc7f1ad1af24a36101f360fa03a1d
Shashwat Mittal <shashwatmittal.india@gmail.com>
2021-10-26 23:33:55 +0530

    updated

--- app/src/main/java/com/example/noteit/addnote.java
+++ app/src/main/java/com/example/noteit/addnote.java
@@ -1,25 +1,19 @@
 package com.example.noteit;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class addnote extends AppCompatActivity {
     Toolbar toolbar;
-    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_addnote);
         toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        recyclerView=findViewById(R.id.list);
-        
     }
-
 
 }
